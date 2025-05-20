95f9991a1c867a0e264a1c04f5aaa8e69bde4029
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2025-05-20 17:48:34 +0900

    (2025-05-20) STL iterator와 알고리즘

---
+++ Ch5_STL/MonsterDatabase.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+
+class Monsters
+{
+
+};
+
+class MonsterDatabase
+{
+private:
+	std::vector<Monsters> monstres;
+	std::vector<int> nums;
+public:
+	void FindMonsterById(int id)
+	{
+		//std::find_if()//
+	}
+
+	void FindNumInDatabase(int x)
+	{
+		//std::find(nuns.begin(), nums.end(),x );
+	}
+
+};
