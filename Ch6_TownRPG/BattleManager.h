14317f63c5db3482c1cbb38f3ca3b68e1a2189e1
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2025-05-21 17:50:01 +0900

    (2025-05-21) 노션을 활용하여 게임 프로젝트 관리하기 TownRPG

---
+++ Ch6_TownRPG/BattleManager.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "GameModel.h"
+
+class BattleManager
+{
+private:
+	Player& player;
+	Monster& monster;
+
+	//BattleManager battlemanager(player,monster);  battleManager.Battle();
+	// 체력이 0보다 크면 반복해라.  -> while 탈출 조건을 먼저 작성해라.
+
+public:
+	void Battle()
+	{
+		while (player.getHealth() > 0 && monster.getHealth() > 0)
+		{
+			player.attack(monster);
+			if (monster.getHealth() > 0)
+			{
+				monster.attack(player);
+			}
+		}
+
+		std::cout << "전투가 종료되었습니다" << std::endl;
+	}
+
+};
